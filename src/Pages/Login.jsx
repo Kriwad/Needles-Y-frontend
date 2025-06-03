b6b9ebff0b7bfa618c29e436749cdf493808fddc
Kriwad <khatiwadakrish84@gmail.com>
2025-06-04 03:21:55 +0400

    prob fixed now plzzz

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import { Button } from "@/components/ui/button.jsx"
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
